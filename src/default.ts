67a93d8d375c0dc5f54345a441c62e477d3c8d01
DennisSkyWind <128893759+DennisSkyWind@users.noreply.github.com>
2023-03-30 15:10:21 +0800

    Add files via upload

---
+++ src/default.ts
@@ -0,0 +1,13 @@
+export const defaultSetting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  systemRule: ""
+}
+
+export const defaultMessage = `
+- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
+- 本站由 [KPCAILAB]搭建。
+- 主要用于学习和研究，并提供给部分有兴趣朋友使用
+- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
